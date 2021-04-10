9d4ad16411e6e65ad0fcc7d6bb1f0e63768d35ad
susankpma <susanm@hawaii.edu>
2021-04-09 20:10:22 -1000

    Collections: courses, profiles, sessions

---
+++ app/imports/api/profiles/Profiles.js
@@ -0,0 +1,34 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/**
+ * The ProfilesCollection. It encapsulates state and variable values for profile.
+ */
+class ProfilesCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ProfilesCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      email: { type: String, index: true, unique: true },
+      firstName: { type: String, optional: true },
+      lastName: { type: String, optional: true },
+      bio: { type: String, optional: true },
+      picture: { type: String, optional: true },
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the ProfilesCollection.
+ * @type {ProfilesCollection}
+ */
+export const Profiles = new ProfilesCollection();
---
+++ app/imports/api/sessions/Sessions.js
@@ -0,0 +1,30 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/**
+ * The SessionsCollection. It encapsulates state and variable values for session.
+ */
+class SessionsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'SessionsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      topic: { type: String, index: true, unique: true },
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the SessionsCollection.
+ * @type {SessionsCollection}
+ */
+export const Sessions = new SessionsCollection();
