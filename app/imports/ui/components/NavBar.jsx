7c8d9a82b4d0a875d369e946747e002710edf2d1
susankpma <susanm@hawaii.edu>
2021-04-14 19:20:45 -1000

    updated Navbar for AddSession

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -18,7 +18,8 @@ class NavBar extends React.Component {
         {this.props.currentUser ? (
           [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
             <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>,
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/calendar" key='list'>Calendar</Menu.Item>]
+            <Menu.Item as={NavLink} activeClassName="active" exact to="/calendar" key='list'>Calendar</Menu.Item>,
+            <Menu.Item as={NavLink} activeClassName="active" exact to="/addsession" key='list'>Add Session</Menu.Item>]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
           <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
