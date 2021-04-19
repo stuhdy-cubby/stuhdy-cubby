3315cbc32f0486bcb7cbf40e66bf0a9d55a79cbf
susankpma <susanm@hawaii.edu>
2021-04-18 21:22:43 -1000

    more updates

--- app/imports/ui/pages/Session.jsx
+++ app/imports/ui/pages/Session.jsx
@@ -8,16 +8,28 @@ import { Profiles } from '../../api/profiles/Profiles';
 import { Sessions } from '../../api/sessions/Sessions';
 import { SessionsProfiles } from '../../api/sessions/SessionsProfiles';
 import { SessionsCourses } from '../../api/sessions/SessionsCourses';
-import { ProfileCard } from '../components/ProfileCard';
 
 /** Returns the Session and associated Profiles */
 function getSessionData(topic) {
   const data = Sessions.collection.findOne({ topic });
-  const sessioncourse = _.pluck(SessionsCourses.collection.find({ topic: topic }).fetch(),'course');
+  const sessioncourse = _.pluck(SessionsCourses.collection.find({ topic: topic }).fetch(), 'course');
   const profiles = _.pluck(SessionsProfiles.collection.find({ topic: topic }).fetch(), 'profile');
   const profilePictures = profiles.map(profile => Profiles.collection.findOne({ email: profile }).picture);
   return _.extend({ }, data, { sessioncourse, participants: profilePictures });
 }
+
+const MakeCard = (props) => (
+  <Card>
+    <Card.Content>
+      <Card.Header style={{ marginTop: '0px' }}>
+        {props.session.topic}
+      </Card.Header>
+      <Card.Description>
+        {props.sessioncourse.location}
+      </Card.Description>
+    </Card.Content>
+  </Card>
+);
 
 /** Renders the Profile Collection as a set of Cards. */
 class Session extends React.Component {
@@ -30,15 +42,15 @@ class Session extends React.Component {
   /** Render the page once subscriptions have been received. */
   renderPage() {
     const sessions = _.pluck(Sessions.collection.find().fetch(), 'topic');
-    
+
     const emails = _.pluck(Profiles.collection.find().fetch(), 'email');
     const profileData = emails.map(email => getProfileData(email));
     return (
-        <Container id="profiles-page">
-          <Card.Group>
-            {_.map(profileData, (profile, index) => <ProfileCard key={index} profile={profile}/>)}
-          </Card.Group>
-        </Container>
+      <Container id="profiles-page">
+        <Card.Group>
+          {_.map(profileData, (profile, index) => <ProfileCard key={index} profile={profile}/>)}
+        </Card.Group>
+      </Container>
     );
   }
 }
