dcf7fee787fab14d648a9926c7f578c1a9dcff5d
susankpma <susanm@hawaii.edu>
2021-04-21 01:11:27 -1000

    updated create session

--- app/imports/ui/pages/AddSession.jsx
+++ app/imports/ui/pages/AddSession.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Grid, Segment, Header, Form } from 'semantic-ui-react';
-import { AutoForm, TextField, DateField, SelectField, LongTextField, SubmitField, ErrorsField } from 'uniforms-semantic';
+import { Grid, Segment, Header, Form, Input } from 'semantic-ui-react';
+import { AutoForm, TextField, DateField, SelectField, LongTextField,
+  HiddenField, SubmitField, ErrorsField } from 'uniforms-semantic';
 import swal from 'sweetalert';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
@@ -15,6 +16,7 @@ import { SessionsCourses } from '../../api/sessions/SessionsCourses';
 /** Create a schema to specify the structure of the data to appear in the form. */
 const makeSchema = (allCourses, allSessions) => new SimpleSchema({
   topic: { type: String, allowedValues: allSessions, optional: false },
+  description: String,
   course: { type: String, allowedValues: allCourses, optional: false },
   location: { type: String, defaultValue: 'ICSpace', label: 'Location' },
   sessionDate: { label: 'Session Date', type: Date, defaultValue: new Date() },
@@ -24,10 +26,23 @@ const makeSchema = (allCourses, allSessions) => new SimpleSchema({
 /** Renders the Page for adding a document. */
 class AddSession extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { description: '' };
+  }
+
+  handleChange(e) {
+    this.setState({ description: e.target.value });
+  }
+
   /** On submit, insert the data. */
   submit(data, formRef) {
-    const { topic, course, location, sessionDate, sessionNotes } = data;
+    const { topic, description, course, location, sessionDate, sessionNotes } = data;
     const owner = Meteor.user().username;
+    if (Sessions.collection.find({ topic: topic }).count() === 0) {
+      Sessions.collection.insert({ topic, description });
+    }
     SessionsCourses.collection.insert({ topic, course, location, sessionDate, sessionNotes, owner },
       (error) => {
         if (error) {
@@ -42,6 +57,7 @@ class AddSession extends React.Component {
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   render() {
     let fRef = null;
+    const descrip = this.state.description;
     const allCourses = _.pluck(Courses.collection.find().fetch(), 'name');
     const allSessions = _.pluck(Sessions.collection.find().fetch(), 'topic');
     const formSchema = makeSchema(allCourses, allSessions);
@@ -52,8 +68,16 @@ class AddSession extends React.Component {
           <Header as="h2" textAlign="center">Add Session</Header>
           <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
             <Segment>
+              <Input size='large' list='topics'
+                label='Topic' name='topics' placeholder='Choose topic...' onChange={this.handleChange}/>
+              <datalist id='topics'>
+                {allSessions.map((option) => (
+                  <option key={option} value={option}>{option}</option>
+                ))}
+              </datalist>
+              <HiddenField name='topic' value={descrip} />
+              <HiddenField name='description' value={descrip} />
               <Form.Group widths={'equal'}>
-                <SelectField id='topic' name='topic' showInlineError={true} placeholder='Topic'/>
                 <SelectField id='course' name='course' showInlineError={true} placeholder='Course'/>
                 <TextField id='location' name='location' showInlineError={true} placeholder='Location'/>
                 <DateField name='sessionDate' showInlineError={true}/>
