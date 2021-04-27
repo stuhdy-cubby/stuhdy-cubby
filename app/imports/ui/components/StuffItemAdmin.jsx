c25e77f999644a22960ac961bbb5326fa70301a2
Christine Ramos <christineramos12@gmail.com>
2021-04-27 11:22:02 -1000

    in progress - implementing SessionsCourses collection to list created sessions

--- app/imports/ui/components/StuffItemAdmin.jsx
+++ app/imports/ui/components/StuffItemAdmin.jsx
@@ -7,10 +7,9 @@ class StuffItemAdmin extends React.Component {
   render() {
     return (
       <Table.Row>
-        <Table.Cell>{this.props.stuff.name}</Table.Cell>
-        <Table.Cell>{this.props.stuff.quantity}</Table.Cell>
-        <Table.Cell>{this.props.stuff.condition}</Table.Cell>
-        <Table.Cell>{this.props.stuff.owner}</Table.Cell>
+        <Table.Cell>{this.props.sessions.topic}</Table.Cell>
+        <Table.Cell>{this.props.sessions.course}</Table.Cell>
+        <Table.Cell>{this.props.sessions.owner}</Table.Cell>
       </Table.Row>
     );
   }
@@ -18,10 +17,9 @@ class StuffItemAdmin extends React.Component {
 
 // Require a document to be passed to this component.
 StuffItemAdmin.propTypes = {
-  stuff: PropTypes.shape({
-    name: PropTypes.string,
-    quantity: PropTypes.number,
-    condition: PropTypes.string,
+  sessions: PropTypes.shape({
+    topic: PropTypes.string,
+    course: PropTypes.string,
     _id: PropTypes.string,
     owner: PropTypes.string,
   }).isRequired,
--- app/imports/ui/pages/AdminHome.jsx
+++ app/imports/ui/pages/AdminHome.jsx
@@ -1,47 +1,31 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Table, Container, Button, Divider, Header } from 'semantic-ui-react';
+import { Table, Container, Button, Divider } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import { Stuffs } from '../../api/stuff/Stuff';
 import StuffItemAdmin from '../components/StuffItemAdmin';
+import { SessionsCourses } from '../../api/sessions/SessionsCourses';
 
 /** A simple static component to render some text for the landing page. */
 class AdminHome extends React.Component {
   render() {
     return (
       <Container id="user-profile">
-        <Header as="h2" textAlign="center">List Stuff (Admin)</Header>
-        <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Name</Table.HeaderCell>
-              <Table.HeaderCell>Quantity</Table.HeaderCell>
-              <Table.HeaderCell>Condition</Table.HeaderCell>
-              <Table.HeaderCell>Owner</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {this.props.stuffs.map((stuff) => <StuffItemAdmin key={stuff._id} stuff={stuff} />)}
-          </Table.Body>
-        </Table>
-
         <h1>My Sessions</h1>
         <p>List of all your current and past sessions.</p>
         <Button as={NavLink} exact to="/addsession" key='list' color='blue'>Add Sessions</Button>
         <Table striped singleLine selectable color='blue'>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Session</Table.HeaderCell>
-              <Table.HeaderCell>Description</Table.HeaderCell>
-              <Table.HeaderCell>Sensei</Table.HeaderCell>
-              <Table.HeaderCell>Students</Table.HeaderCell>
+              <Table.HeaderCell>Topic</Table.HeaderCell>
+              <Table.HeaderCell>Course</Table.HeaderCell>
+              <Table.HeaderCell>Owner</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
 
           <Table.Body>
-            {this.props.stuffs.map((stuff) => <StuffItemAdmin key={stuff._id} stuff={stuff} />)}
+            {this.props.sessions.map((session) => <StuffItemAdmin key={session._id} session={session} />)}
           </Table.Body>
         </Table>
 
@@ -53,15 +37,14 @@ class AdminHome extends React.Component {
         <Table striped singleLine selectable color='brown'>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Session</Table.HeaderCell>
-              <Table.HeaderCell>Description</Table.HeaderCell>
-              <Table.HeaderCell>Sensei</Table.HeaderCell>
-              <Table.HeaderCell>Students</Table.HeaderCell>
+              <Table.HeaderCell>Topic</Table.HeaderCell>
+              <Table.HeaderCell>Course</Table.HeaderCell>
+              <Table.HeaderCell>Owner</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
 
           <Table.Body>
-            {this.props.stuffs.map((stuff) => <StuffItemAdmin key={stuff._id} stuff={stuff} />)}
+            {this.props.sessions.map((session) => <StuffItemAdmin key={session._id} session={session} />)}
           </Table.Body>
         </Table>
       </Container>
@@ -70,20 +53,20 @@ class AdminHome extends React.Component {
 }
 
 AdminHome.propTypes = {
-  stuffs: PropTypes.array.isRequired,
+  sessions: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Stuffs.adminPublicationName);
+  const subscription = Meteor.subscribe(SessionsCourses.adminPublicationName);
   // Determine if the subscription is ready
   const ready = subscription.ready();
   // Get the Stuff documents
-  const stuffs = Stuffs.collection.find({}).fetch();
+  const sessions = SessionsCourses.collection.find({}).fetch();
   return {
-    stuffs,
+    sessions,
     ready,
   };
 })(AdminHome);
