c6a58f61fbe85266f97485bf580ebcd069284c94
susankpma <susanm@hawaii.edu>
2021-04-26 23:28:28 -1000

    modified link to register session

--- app/imports/ui/pages/AddSession.jsx
+++ app/imports/ui/pages/AddSession.jsx
@@ -41,22 +41,23 @@ class AddSession extends React.Component {
     const { topic, description, course, location, sessionDate, sessionNotes } = data;
     const emailList = ['susankpma@gmail.com', 'susanm@hawaii.edu'];
     const subjectLine = `New session for: ${topic}`;
-    const emailMessage = `<b>Topic:</b> ${topic}<br/>
-    <b>Course:</b> ${course}<br/>
-    <b>Location:</b> ${location}<br/>
-    <b>Date and Time:</b> ${sessionDate}<br/>
-    <b>Notes:</b> ${sessionNotes}<br/>
-    <a href="#">Register for this session</a>`;
     const owner = Meteor.user().username;
     if (Sessions.collection.find({ topic: topic }).count() === 0) {
       Sessions.collection.insert({ topic, description });
     }
     SessionsCourses.collection.insert({ topic, course, location, sessionDate, sessionNotes, owner },
-      (error) => {
+      (error, docsInserted) => {
         if (error) {
           swal('Error', error.message, 'error');
         } else {
+          const emailMessage = `<b>Topic:</b> ${topic}<br/>
+    <b>Course:</b> ${course}<br/>
+    <b>Location:</b> ${location}<br/>
+    <b>Date and Time:</b> ${sessionDate}<br/>
+    <b>Notes:</b> ${sessionNotes}<br/>
+    <a href="/#/registersession/${docsInserted}">Register for this session</a>`;
           swal('Success', 'stUHdy session added successfully', 'success');
+          console.log(docsInserted);
           /** Email registered users of new session */
 
           _.each(emailList, function (n) {
