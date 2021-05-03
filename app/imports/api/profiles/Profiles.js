ff5cbf7d13419d833d50d88fcc7d8f153c2ea796
Christine Ramos <christineramos12@gmail.com>
2021-05-02 17:20:29 -1000

    included interests and skills into create profile form

--- app/imports/api/profiles/Profiles.js
+++ app/imports/api/profiles/Profiles.js
@@ -30,6 +30,10 @@ class ProfilesCollection {
         type: String,
         allowedValues: ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Other'],
         optional: true },
+      interests: { type: String },
+      // enrolledCourses: { type: String },
+      // previouslyEnrolledCourses: { type: String },
+      skills: { type: String },
     }, { tracker: Tracker });
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
--- app/imports/ui/pages/UserInfo.jsx
+++ app/imports/ui/pages/UserInfo.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Grid, Segment, Header, Divider, Button } from 'semantic-ui-react';
+import { Grid, Segment, Header, Divider, Checkbox, FormField } from 'semantic-ui-react';
 import { AutoForm, ErrorsField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
 import PropTypes from 'prop-types';
-import { NavLink, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+import { GraphQLIncludeDirective } from 'graphql';
 import { Profiles } from '../../api/profiles/Profiles';
 
 // Create a schema to specify the structure of the data to appear in the form.
@@ -14,7 +15,7 @@ const formSchema = new SimpleSchema({
   email: { type: String, index: true, unique: true },
   firstName: { type: String },
   lastName: { type: String },
-  bio: { type: String, optional: true },
+  bio: { type: String },
   picture: { type: String, optional: true },
   institution: {
     type: String,
@@ -28,6 +29,10 @@ const formSchema = new SimpleSchema({
     type: String,
     allowedValues: ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Other'],
   },
+  interests: { type: String },
+  // enrolledCourses: { type: String },
+  // previouslyEnrolledCourses: { type: String },
+  skills: { type: String },
 });
 
 const bridge = new SimpleSchema2Bridge(formSchema);
@@ -37,9 +42,9 @@ class UserInfo extends React.Component {
 
   // On submit, insert the data.
   submit(data, formRef) {
-    const { firstName, lastName, email, institution, major, standing, bio } = data;
+    const { firstName, lastName, email, institution, major, standing, bio, interests, skills } = data;
     const owner = Meteor.user().username;
-    Profiles.collection.insert({ firstName, lastName, email, institution, major, standing, bio, owner },
+    Profiles.collection.insert({ firstName, lastName, email, institution, major, standing, bio, owner, interests, skills },
       (error) => {
         if (error) {
           swal('Error', error.message, 'error');
@@ -68,7 +73,69 @@ class UserInfo extends React.Component {
               <SelectField name='major'/>
               <SelectField name='standing'/>
               <TextField name='bio' />
-              <SubmitField value='Submit' as={NavLink} activeClassName="active" exact to="/profile"/>
+
+              <Divider hidden />
+              <Divider clearing />
+              <Divider hidden />
+
+              <p><strong>Interests</strong></p>
+              <p>Select all that apply.</p>
+              <Grid columns='three'>
+                <Grid.Row>
+                  <Grid.Column width={3}>
+                    <Checkbox label={<label>Art</label>} />
+                    <Divider hidden />
+                    <Checkbox label={<label>Culinary Arts</label>} />
+                  </Grid.Column>
+                  <Grid.Column width={3}>
+                    <Checkbox label={<label>Music</label>} />
+                    <Divider hidden />
+                    <Checkbox label={<label>Athletics</label>} />
+                  </Grid.Column>
+                  <Grid.Column width={3}>
+                    <Checkbox label={<label>Creative Media</label>} />
+                    <Divider hidden />
+                    <Checkbox label={<label>Technology</label>} />
+                  </Grid.Column>
+                  <Grid.Column width={3}>
+                    <Checkbox label={<label>Other</label>} />
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+
+              <Divider hidden />
+              <Divider clearing />
+              <Divider hidden />
+
+              <p><strong>Skills</strong></p>
+              <p>Select all that apply.</p>
+              <Grid columns='three'>
+                <Grid.Row>
+                  <Grid.Column width={2}>
+                    <Checkbox label={<label>Javascript</label>} />
+                    <Divider hidden />
+                    <Checkbox label={<label>Java</label>} />
+                  </Grid.Column>
+                  <Grid.Column width={2}>
+                    <Checkbox label={<label>HTML</label>} />
+                    <Divider hidden />
+                    <Checkbox label={<label>CSS</label>} />
+                  </Grid.Column>
+                  <Grid.Column width={2}>
+                    <Checkbox label={<label>C</label>} />
+                    <Divider hidden />
+                    <Checkbox label={<label>C++</label>} />
+                  </Grid.Column>
+                  <Grid.Column width={2}>
+                    <Checkbox label={<label>Other</label>}/>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+
+              <Divider hidden />
+              <Divider hidden />
+
+              <SubmitField value='Submit'/>
               <ErrorsField/>
             </Segment>
           </AutoForm>
