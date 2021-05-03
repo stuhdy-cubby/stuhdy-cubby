fc57338a37bd004fe19a4bc0ed6b2f1536626e7f
susankpma <susanm@hawaii.edu>
2021-05-02 14:06:42 -1000

    leaderboard

--- app/imports/ui/pages/LeaderboardPage.jsx
+++ app/imports/ui/pages/LeaderboardPage.jsx
@@ -3,8 +3,9 @@ import { Meteor } from 'meteor/meteor';
 import { Container, Table, Header, Loader } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { _ } from 'meteor/underscore';
 import Leaderboard from '../components/Leaderboard';
-import { Profiles} from '../../api/profiles/Profiles';
+import { Profiles } from '../../api/profiles/Profiles';
 import { ProfilesPoints } from '../../api/profiles/ProfilesPoints';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
@@ -17,6 +18,17 @@ class LeaderboardPage extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
+    const data = ProfilesPoints.collection.find({}, { sort: { points: -1 } }).fetch();
+    const person = _.groupBy(data, 'profile');
+    const personObj = {};
+    _.each(person, function (val, key) {
+      personObj[key] = _.reduce(val, function (memo, v) {
+        return memo + v.points;
+      }, 0);
+    });
+    const profilePoints = _.sortBy(_.map(_.keys(personObj), function (s) {
+      return { profile: s, points: personObj[s] };
+    }), 'points').reverse();
     return (
       <Container>
         <Header as="h2" textAlign="center">Leaderboard</Header>
@@ -28,7 +40,7 @@ class LeaderboardPage extends React.Component {
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {this.props.profiles.map((leaderboard) => <Leaderboard key={leaderboard._id} leaderboard={leaderboard}/>)}
+            {profilePoints.map((leaderboard) => <Leaderboard key={leaderboard.profile} leaderboard={leaderboard}/>)}
           </Table.Body>
         </Table>
       </Container>
